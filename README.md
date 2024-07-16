db2a6236aa44fdc1e58e7d2817787d7475f1b7c9
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-07-16 10:11:51 +0700

    update docs

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
 # Token Adapter
 
 Support `ICRC1` `ICRC2` `EXT` `DIIP20`
+
+### Token interface
+
+```typescript
+export type ApproveInput = {
+  fee: [] | [bigint];
+  memo: [] | [Uint8Array | number[]];
+  from_subaccount: [] | [Uint8Array | number[]];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+  expected_allowance: [] | [bigint];
+  expires_at: [] | [bigint];
+  spender: {
+    owner: Principal;
+    subaccount: [] | [Uint8Array | number[]];
+  };
+};
+
+export type TransferInput = {
+  to: {
+    owner: Principal;
+    subaccount: [] | [Uint8Array | number[]];
+  };
+  amount: bigint;
+  fee: [bigint] | [];
+  memo: [Uint8Array | number[]] | [];
+  from_subaccount: [] | [Uint8Array | number[]];
+  created_at_time: [bigint] | [];
+};
+
+export interface IToken {
+  actor: TokenActor;
+  getDecimals(): Promise<number>;
+  balanceOf(
+    address:
+      | string
+      | {
+          owner: Principal;
+          subaccount: [] | [Uint8Array | number[]];
+        }
+  ): Promise<number>;
+  name(): Promise<string>;
+  symbol(): Promise<string>;
+  totalSupply(): Promise<number>;
+  getFee(): Promise<bigint>;
+  getMetadata(): Promise<any>;
+  getLogo(): Promise<string>;
+  /**
+   *
+   *  @description This method only work with EXT, ICRC2+ and DIP20 tokens
+   */
+  approve(input: ApproveInput): Promise<bigint>;
+
+  getFee(): Promise<bigint>;
+  transfer(input: TransferInput): Promise<bigint>;
+}
+```
--- src/types/IToken.ts
+++ src/types/IToken.ts
@@ -1,6 +1,33 @@
 import { Principal } from "@dfinity/principal";
 import { TokenActor } from "./TokenStandardActor";
 
+export type ApproveInput = {
+  fee: [] | [bigint];
+  memo: [] | [Uint8Array | number[]];
+  from_subaccount: [] | [Uint8Array | number[]];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+  expected_allowance: [] | [bigint];
+  expires_at: [] | [bigint];
+  spender: {
+    owner: Principal;
+    subaccount: [] | [Uint8Array | number[]];
+  };
+}
+
+export type TransferInput = {
+  to: {
+    owner: Principal;
+    subaccount: [] | [Uint8Array | number[]];
+  };
+  amount: bigint;
+  fee: [bigint] | [];
+  memo: [Uint8Array | number[]] | [];
+  from_subaccount: [] | [Uint8Array | number[]];
+  created_at_time: [bigint] | [];
+}
+
+
 export interface IToken {
   actor: TokenActor;
   getDecimals(): Promise<number>;
@@ -22,30 +49,8 @@ export interface IToken {
    * 
    *  @description This method only work with EXT, ICRC2+ and DIP20 tokens
    */
-  approve(input: {
-    fee: [] | [bigint];
-    memo: [] | [Uint8Array | number[]];
-    from_subaccount: [] | [Uint8Array | number[]];
-    created_at_time: [] | [bigint];
-    amount: bigint;
-    expected_allowance: [] | [bigint];
-    expires_at: [] | [bigint];
-    spender: {
-      owner: Principal;
-      subaccount: [] | [Uint8Array | number[]];
-    };
-  }): Promise<bigint>;
+  approve(input: ApproveInput): Promise<bigint>;
 
   getFee(): Promise<bigint>;
-  transfer(input: {
-    to: {
-      owner: Principal;
-      subaccount: [] | [Uint8Array | number[]];
-    };
-    amount: bigint;
-    fee: [bigint] | [];
-    memo: [Uint8Array | number[]] | [];
-    from_subaccount: [] | [Uint8Array | number[]];
-    created_at_time: [bigint] | [];
-  }): Promise<bigint>;
+  transfer(input: TransferInput): Promise<bigint>;
 }

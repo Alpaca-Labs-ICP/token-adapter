5cd79b442fb67cdac2571afaa4dc0db4a54aba6c
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-09-04 23:38:04 +0700

    add dist folder

---
+++ dist/dip20.js
@@ -0,0 +1,94 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Dip20Token = void 0;
+const principal_1 = require("@dfinity/principal");
+const icp_1 = require("./icp");
+class Dip20Token {
+    constructor({ canisterId, agent, }) {
+        this.decimals = 1e8;
+        this.actor = icp_1.DIP20.createActor({
+            agent,
+            canisterId,
+        });
+    }
+    approve(input) {
+        console.log("approve", input);
+        throw new Error("Method not implemented.");
+    }
+    transfer(input) {
+        console.log("transfer", input);
+        throw new Error("Method not implemented.");
+    }
+    getDecimals() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.actor.decimals();
+        });
+    }
+    balanceOf(address) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const balance = yield this.actor.balanceOf(principal_1.Principal.fromText(address));
+            return Number(balance) / this.decimals;
+        });
+    }
+    name() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.actor.name();
+        });
+    }
+    symbol() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.actor.symbol();
+        });
+    }
+    totalSupply() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const totalSupplyBigInt = yield this.actor.totalSupply();
+            return Number(totalSupplyBigInt) / this.decimals;
+        });
+    }
+    getFee() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.actor.getTokenFee();
+        });
+    }
+    getBurnedAmountInDeadWallet() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const burnedAmount = yield this.balanceOf("aaaaa-aa");
+            return Number(burnedAmount);
+        });
+    }
+    getBurnedAmmount() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const burnedDeadWallet = yield this.getBurnedAmountInDeadWallet();
+            return burnedDeadWallet;
+        });
+    }
+    holdPercentage(address) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const totalSupply = yield this.totalSupply();
+            const balance = yield this.balanceOf(address);
+            return balance / totalSupply;
+        });
+    }
+    getMetadata() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.actor.getMetadata();
+        });
+    }
+    getLogo() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const logo = yield this.actor.logo();
+            return logo;
+        });
+    }
+}
+exports.Dip20Token = Dip20Token;
---
+++ dist/ext.js
@@ -0,0 +1,141 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ExtToken = void 0;
+const principal_1 = require("@dfinity/principal");
+const icp_1 = require("./icp");
+class ExtToken {
+    constructor({ canisterId, agent, }) {
+        this.decimals = 1e8;
+        this.actor = icp_1.EXT.createActor({
+            agent,
+            canisterId,
+        });
+    }
+    // TODO: Implement the following methods
+    approve(input) {
+        console.log("approve", input);
+        throw new Error("Method not implemented.");
+    }
+    transfer(input) {
+        console.log("transfer", input);
+        throw new Error("Method not implemented.");
+    }
+    getDecimals() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const metadata = yield this.actor.metadata();
+            if ("ok" in metadata && "fungible" in metadata.ok) {
+                return metadata.ok.fungible.decimals;
+            }
+            else {
+                throw new Error("getDecimals error occured");
+            }
+        });
+    }
+    balanceOf(address) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const balance = yield this.actor.balance({
+                token: "",
+                user: {
+                    principal: principal_1.Principal.fromText(address),
+                },
+            });
+            return Number(balance) / this.decimals;
+        });
+    }
+    name() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const metadata = yield this.actor.metadata();
+            if ("ok" in metadata && "fungible" in metadata.ok) {
+                return metadata.ok.fungible.name;
+            }
+            else if ("err" in metadata) {
+                throw new Error("error occured");
+            }
+            throw new Error("error occured");
+        });
+    }
+    symbol() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const metadata = yield this.actor.metadata();
+            if ("ok" in metadata && "fungible" in metadata.ok) {
+                return metadata.ok.fungible.symbol;
+            }
+            else if ("err" in metadata) {
+                console.log(metadata.err);
+                throw new Error("error occured");
+            }
+            throw new Error("error occured");
+        });
+    }
+    totalSupply() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const totalSupplyBigInt = yield this.actor.supply();
+            if ("err" in totalSupplyBigInt) {
+                console.log(totalSupplyBigInt.err);
+                throw new Error("error occured");
+            }
+            if ("ok" in totalSupplyBigInt) {
+                return Number(totalSupplyBigInt.ok) / this.decimals;
+            }
+            throw new Error("error occured");
+        });
+    }
+    getFee() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const fee = yield this.actor.getFee();
+            if ("err" in fee) {
+                console.log(fee.err);
+                throw new Error("error occured");
+            }
+            if ("ok" in fee) {
+                return fee.ok;
+            }
+            throw new Error("error occured");
+        });
+    }
+    getBurnedAmountInDeadWallet() {
+        return __awaiter(this, void 0, void 0, function* () {
+            throw new Error("Method not implemented.");
+        });
+    }
+    getBurnedAmmount() {
+        return __awaiter(this, void 0, void 0, function* () {
+            throw new Error("Method not implemented.");
+        });
+    }
+    holdPercentage(address) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const totalSupply = yield this.totalSupply();
+            const balance = yield this.balanceOf(address);
+            return balance / totalSupply;
+        });
+    }
+    getMetadata() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const metadata = yield this.actor.metadata();
+            if ("ok" in metadata) {
+                return metadata.ok;
+            }
+            throw new Error("error occured");
+        });
+    }
+    getLogo() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const result = yield this.actor.logo();
+            if ("ok" in result) {
+                return result.ok;
+            }
+            throw new Error("error occured");
+        });
+    }
+}
+exports.ExtToken = ExtToken;

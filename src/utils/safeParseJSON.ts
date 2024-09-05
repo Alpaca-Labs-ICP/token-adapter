0fda87f82a00e27233902249b3f2557a2553e3d3
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-09-06 01:37:02 +0700

    add return for safeParseJSON

--- src/utils/safeParseJSON.ts
+++ src/utils/safeParseJSON.ts
@@ -1,5 +1,5 @@
 export const safeParseJSON = (arg: Record<string, unknown>): any => {
-  JSON.stringify(
+  return JSON.stringify(
     arg,
     (key, value) => (typeof value === "bigint" ? value.toString() : value) // return everything else unchanged
   );
